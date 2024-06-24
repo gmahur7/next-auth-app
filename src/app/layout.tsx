84aff971f90c58c10a3d2dfb3226cf85f3573db2
gmahur7 <gm7017308@gmail.com>
2024-06-24 09:48:00 +0530

    again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import 'react-toastify/dist/ReactToastify.css';
 import Header from "@/components/Header";
+import { Suspense } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Suspense fallback={<div>Loading...</div>}>
         <Header/>
         {children}
+        </Suspense>
       </body>
     </html>
   );
