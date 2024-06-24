632b2dfbaa6a471bc8071883a305d4dbbd5a9ab8
gmahur7 <gm7017308@gmail.com>
2024-06-24 10:21:48 +0530

    again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-20 sm:p-24 sm:my-16">
       <div className="w-full flex flex-col gap-4 sm:gap-4 lg:gap-0 items-center sm:flex-row px-4 sm:px-16 py-5 sm:justify-evenly">
         <div className="sm:order-2">
-          <Image src='/images/user.png' alt="image" height={100} width={200} layout="responsive" />
+          <Image src='/images/user.png' alt="image" height={100} width={200}  />
         </div>
         <div>
           <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl">Welcome <span className="text-blue-500 font-bold text-2xl sm:text-5xl">{data}</span></h2>
